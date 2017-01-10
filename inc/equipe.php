ff3fbf280028f4381a531cd673259f2c7f69dbd4
IRONBAN <heramban_minatchy@hotmail.com>
2017-01-10 15:56:49 +0100

    titre save

---
+++ inc/equipe.php
@@ -0,0 +1,20 @@
+	<body>
+		<section id="equipe">
+			<article>
+				<img src="" alt="Pierre">
+				<p>Pierre</p>
+			</article>
+			<article>
+				<img src="" alt="Sonny">
+				<p>Sonny</p>
+			</article>
+			<article>
+				<img src="" alt="Heramban">
+				<p>Heramban</p>
+			</article>
+			<article>	
+				<img src="" alt="Yann">
+				<p>Yann</p>
+			<article>
+		</section>	
+	</body>
---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<h1>Webforce Beurk!!</h1>
+					<nav>
+						<ul>	
+							<li><a href="#presentation">Présentation</a></li>
+							<li><a href="#equipe">L'équipe</a></li>
+							<li><a href="#reserve">Réservation</a></li>
+						</ul>	
+					</nav>	
