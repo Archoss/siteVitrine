b972ab76c9f2851ef072c84a8c5537c68cb54f99
IRONBAN <heramban_minatchy@hotmail.com>
2017-01-10 15:22:50 +0100

    Update footer.php

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+			<footer class="footer">
+			<!-- Google Map -->
+				<div class="googlemap">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21004.94967718324!2d2.33242335!3d48.8464112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1477495240795" width="300" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+
+			<!-- Contactez-Nous -->
+
+			<h3>Contactez-nous</h3>
+
+				<div class="contact">
+					 <address>
+						<a href="tel:+123456789" title="Téléphone">Téléphone : 01 69 67 66 65</a><br>
+
+						<a href=">wf3Restaurant@gmail.fr" title="Adresse E-mail">wf3Restaurant@gmail.fr</a><br>
+						<p>82, avenue Denfert-Rochereau,</p>
+						<p>75014 Paris, France</p><br>
+						
+					</address>
+				</div>
+
+			</footer>
